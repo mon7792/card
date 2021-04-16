b40cf250f49185dfe40cfecccaf27f392da52ba7
mon7792 <monish7792@gmail.com>
2021-04-16 18:12:02 +0100

    added file to un-marshal the message sent by the client.

---
+++ services/game/message.go
@@ -0,0 +1,20 @@
+// package game serves the game backend
+package game
+
+import "encoding/json"
+
+type message struct {
+	GameID   string `json:"game-id"`
+	PlayerID string `json:"player-id"`
+	Message  string `json:"message"`
+}
+
+// DecodeMessage unmarshal the message and check if they are in proper format.
+func DecodeMessage(data []byte) (*message, error) {
+	var msg *message
+
+	if err := json.Unmarshal(data, &msg); err != nil {
+		return nil, err
+	}
+	return msg, nil
+}
