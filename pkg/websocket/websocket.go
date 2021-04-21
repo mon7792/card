58e027f7125238f7e62a0e4a85e57139bbc64d62
mon7792 <monish7792@gmail.com>
2021-04-21 18:16:46 +0100

    updated websocket to use appropriate handler

---
+++ pkg/websocket/websocket.go
@@ -0,0 +1,52 @@
+// package websocket
+package websocket
+
+import (
+	"log"
+	
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+// TODO: Work on this
+//https://tutorialedge.net/projects/chat-system-in-go-and-react/part-4-handling-multiple-clients/
+
+// upGrader docs.
+var upGrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	}}
+
+// Upgrader docs
+func Upgrader(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error){
+	ws, err := upGrader.Upgrade(w,r, nil)
+	if err != nil {
+		log.Println(err)
+		return ws, err
+	}
+
+	return ws, nil
+}
+
+// Reader docs
+func Reader(conn *websocket.Conn,handleMessage func (message []byte) error){
+	for {
+		mt, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("message:", string(message))
+		if err := handleMessage(message); err != nil {
+			log.Println(err)
+			return
+		}
+
+		if err := conn.WriteMessage(mt, message); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
--- services/game/game.go
+++ services/game/game.go
@@ -3,9 +3,12 @@ package game
 
 import (
 	"log"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
+
+	webSoc "github.com/mon7792/card/pkg/websocket"
 )
 
 // upgrader specify the parameter to upgrade from http_connection to websocket connection
@@ -26,49 +29,66 @@ func ServeGame() {
 		}
 	})
 	//  serve the websocket connection
-	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/echo", serveWs)
 
 	if err := http.ListenAndServe(":5000", nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
-// 2. create a websocket handler
-func echo(w http.ResponseWriter, r *http.Request) {
-	//  upgrade the connection
-	//  here conn refer the websocket connection
-
-	conn, err := upgrader.Upgrade(w, r, nil)
+func serveWs(w http.ResponseWriter, r *http.Request){
+	ws, err := webSoc.Upgrader(w, r)
 	if err != nil {
-		log.Println("err:", err)
+		fmt.Fprintf(w, "%+V\n", err)
 		return
 	}
-	defer func() {
-		if err := conn.Close(); err != nil {
-			log.Println(err)
-		}
-	}()
-
-	log.Printf("local address: %s  || remote address: %s \n", conn.LocalAddr(), conn.RemoteAddr())
-	// read the message and write the message
-	for {
-		mt, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("err:", err)
-			break
-		}
-		log.Printf("recv: %s \n", string(message))
-		result, err := DecodeMessage(message)
-		if err != nil {
-			log.Println("err:", err)
-		}
-		log.Printf("recv: %v \n", result)
-
-		err = conn.WriteMessage(mt, message)
-		if err != nil {
-			log.Println("err:", err)
-			break
-		}
-	}
-
+	webSoc.Reader(ws, handleMessage)
 }
+func handleMessage(message []byte)error {
+	result, err := DecodeMessage(message)
+		if err != nil {
+			log.Println("err:", err)
+			return err
+		}
+		log.Println(result)
+		return nil
+}
+// 2. create a websocket handler
+// func echo(w http.ResponseWriter, r *http.Request) {
+// 	//  upgrade the connection
+// 	//  here conn refer the websocket connection
+
+// 	conn, err := upgrader.Upgrade(w, r, nil)
+// 	if err != nil {
+// 		log.Println("err:", err)
+// 		return
+// 	}
+// 	defer func() {
+// 		if err := conn.Close(); err != nil {
+// 			log.Println(err)
+// 		}
+// 	}()
+
+// 	log.Printf("local address: %s  || remote address: %s \n", conn.LocalAddr(), conn.RemoteAddr())
+// 	// read the message and write the message
+// 	for {
+// 		mt, message, err := conn.ReadMessage()
+// 		if err != nil {
+// 			log.Println("err:", err)
+// 			break
+// 		}
+// 		log.Printf("recv: %s \n", string(message))
+// 		result, err := DecodeMessage(message)
+// 		if err != nil {
+// 			log.Println("err:", err)
+// 		}
+// 		log.Printf("recv: %v \n", result)
+
+// 		err = conn.WriteMessage(mt, message)
+// 		if err != nil {
+// 			log.Println("err:", err)
+// 			break
+// 		}
+// 	}
+
+// }
