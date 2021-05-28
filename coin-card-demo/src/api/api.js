cfe6b263b944bcc4ae43f4637eb1549abfce0a4e
mon7792 <monish7792@gmail.com>
2021-05-28 18:16:53 +0100

    added html page.

--- coin-card-demo/src/api/api.js
+++ coin-card-demo/src/api/api.js
@@ -1,17 +1,18 @@
-// const express = require('express');
-// const app = express();
-const port = 3000;
-//
-//
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
-//
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`)
-// })
+import express from 'express'
 
+export class Api {
+    constructor(port){
+        this.port = port;
+        this.app = express()
+    }
 
-export function api() {
-    console.log(`api function ${port}`)
+    listenAndServe(){
+        return this.app.listen(this.port, () => {
+            console.log(`Example app listening at http://localhost:${this.port}`)
+        })
+    }
+
+    addRoute(route, handler){
+        this.app.get(route, handler)
+    }
 }
--- coin-card-demo/src/app.js
+++ coin-card-demo/src/app.js
@@ -1,4 +1,10 @@
-import {api} from './api/api';
-
-api();
+import { Api} from './api/api';
+import path from 'path';
+import express from 'express'
 console.log('coin-card-demo');
+
+
+let ap = new Api(3000);
+ap.app.use('/static', express.static(path.join(__dirname, 'ui/static')));
+ap.addRoute('/', (req, res) => res.sendFile(path.join(__dirname, 'ui/index.html')));
+ap.listenAndServe();
