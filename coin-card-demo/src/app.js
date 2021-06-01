129e41593f730b2e8339e75e91a6a7b3bf50bddc
mon7792 <monish7792@gmail.com>
2021-06-01 17:15:08 +0100

    renamed the folder structure

--- coin-card-demo/src/app.js
+++ coin-card-demo/src/app.js
@@ -5,6 +5,6 @@ console.log('coin-card-demo');
 
 
 let ap = new Api(3000);
-ap.app.use('/static', express.static(path.join(__dirname, 'ui/static')));
-ap.addRoute('/', (req, res) => res.sendFile(path.join(__dirname, 'ui/index.html')));
+ap.app.use('/static', express.static(path.join(__dirname, 'dist/static')));
+ap.addRoute('/', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')));
 ap.listenAndServe();
