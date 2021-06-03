48bced103a1a4b8c65b00b6c28988d9668623499
mon7792 <monish7792@gmail.com>
2021-06-03 17:49:01 +0100

    completed the basic stake game.

--- coin-card-demo/src/dist/static/index.js
+++ coin-card-demo/src/dist/static/index.js
@@ -19,26 +19,69 @@ function determineWinner() {
 
   return player2.name;
 } // ui-function
+// player 1
 
 
 var player1Input = document.getElementById("player-1-stake-input-id");
 var player1StakeButton = document.getElementById("player-1-stake-button-id");
+var player1CoinField = document.getElementById("player-1-coin-id"); // player 2
+
 var player2Input = document.getElementById("player-2-stake-input-id");
 var player2StakeButton = document.getElementById("player-2-stake-button-id");
+var player2CoinField = document.getElementById("player-2-coin-id"); // game
+
+var newGameButton = document.getElementById('new-game-button-id');
 var resultCheckButton = document.getElementById('result-check-button-id');
+var resultDisplay = document.getElementById('result-id');
+var potDisplay = document.getElementById('current-pot-size-id'); // newGameButton starts the new game.
+
+newGameButton.addEventListener('click', function () {
+  // disable the current button
+  newGameButton.disabled = true; // enable player area
+
+  enablePlayerArea(true);
+  resultCheckButton.disabled = false;
+});
 player1StakeButton.addEventListener('click', function () {
-  var playerStake = parseInt(player1Input.value);
+  var playerInput = player1Input.value.trim();
+
+  if (playerInput === "") {
+    return;
+  }
+
+  var playerStake = parseInt(playerInput); // decrement player coin
+
   player1.coin -= playerStake;
-  pot += playerStake;
-  player1Input.value = '';
+  player1Input.value = ''; // update the pot
+
+  pot += playerStake; // display
+
+  updateDisplayCurrentPot(pot);
+  updateDisplayPlayer1Coin(player1.coin);
 });
 player2StakeButton.addEventListener('click', function () {
-  var playerStake = parseInt(player2Input.value);
+  var playerInput = player2Input.value.trim();
+
+  if (playerInput === "") {
+    return;
+  }
+
+  var playerStake = parseInt(playerInput); // decrement player coin
+
   player2.coin -= playerStake;
-  pot += playerStake;
-  player2Input.value = '';
+  player2Input.value = ''; // update the pot
+
+  pot += playerStake; // display
+
+  updateDisplayCurrentPot(pot);
+  updateDisplayPlayer2Coin(player2.coin);
 });
 resultCheckButton.addEventListener('click', function () {
+  if (pot === 0) {
+    return;
+  } // determine winner
+
+
   var winner = determineWinner();
 
   if (winner === player1.name) {
@@ -47,7 +90,46 @@ resultCheckButton.addEventListener('click', function () {
     player2.coin += pot;
   }
 
-  pot = 0;
-  console.log(player1);
-  console.log(player2);
-});
+  pot = 0; // DISPLAY
+
+  enablePlayerArea(false);
+  updateDisplayCurrentPot(pot);
+  updateDisplayPlayer1Coin(player1.coin);
+  updateDisplayPlayer2Coin(player2.coin);
+  updateDisplayResult(winner);
+  newGameButton.disabled = false;
+  resultCheckButton.disabled = true;
+}); // function
+
+function updateDisplayCurrentPot(coin) {
+  potDisplay.innerHTML = coin.toString();
+}
+
+function updateDisplayPlayer1Coin(coin1) {
+  player1CoinField.innerHTML = coin1.toString();
+}
+
+function updateDisplayPlayer2Coin(coin2) {
+  player2CoinField.innerHTML = coin2.toString();
+}
+
+function updateDisplayResult(playerName) {
+  resultDisplay.innerText = "".concat(playerName, " won the session.");
+}
+
+function enablePlayerArea(enable) {
+  player1Input.disabled = !enable;
+  player1StakeButton.disabled = !enable;
+  player2Input.disabled = !enable;
+  player2StakeButton.disabled = !enable;
+}
+
+window.onload = function () {
+  console.log("WINDOW LOADED"); // INITIAL BOARD DISPLAY
+
+  updateDisplayCurrentPot(pot);
+  updateDisplayPlayer1Coin(player1.coin);
+  updateDisplayPlayer2Coin(player2.coin);
+  enablePlayerArea(false);
+  resultCheckButton.disabled = true;
+};
